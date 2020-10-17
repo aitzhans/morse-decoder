c16b0fc114833dec2de57746034d586ce7443fcb
aitzhans <aitzhan@mail.ru>
2020-10-17 22:06:55 +0600

    feat: solve morse-decoder task

--- src/index.js
+++ src/index.js
@@ -39,6 +39,28 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    let encodedLetter = '';
+    let decodedLetter = '';
+    let result = '';
+    let indexOf1;
+    for (let i = 0; i < expr.length; i += 10) {
+        decodedLetter = '';
+        encodedLetter = expr.substr(i, 10);
+        console.log(encodedLetter);
+        if (encodedLetter === '**********') {
+            result += ' ';
+        } else {
+            while (encodedLetter.length > 1) {
+                indexOf1 = encodedLetter.indexOf('1');
+                decodedLetter = (encodedLetter.substr(indexOf1, 2) === '10') ? decodedLetter + '.' : decodedLetter + '-';
+                encodedLetter = encodedLetter.slice(indexOf1 + 2);
+                console.log(encodedLetter);     
+            }
+            console.log(decodedLetter);
+            result += MORSE_TABLE[decodedLetter];
+        }
+    }
+    return result;
 }
 
 module.exports = {
